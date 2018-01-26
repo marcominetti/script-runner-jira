fa582f59f82b47070f174d241a8266c8a936dd62
Marco Minetti <marco.minetti@novetica.org>
2018-01-26 18:27:34 +0100

    updated warning field with hints

--- compound.warnings.groovy
+++ compound.warnings.groovy
@@ -29,88 +29,93 @@ def createAnomaly(List<Map<String,String>> anomalies, Issue issue, String level,
     anomaly.put("description", description)
     anomaly.put("issue", issue.getKey())
     anomaly.put("level", level)
-    anomaly.put("resolution", issue.getResolution() != null)
+    anomaly.put("resolution", (issue.getResolution() != null)?"true":"false")
     anomaly.put("status", issue.getStatus().getName())
     anomaly.put("type", issue.getIssueType().getName())
     anomalies.add(anomaly)
 }
 
 def calculateAnomalies(Issue issue, List circularityCache, IssueLinkManager issueLinkManager, Logger log, List<Map<String,String>> anomalies) {
-    String thisKey = issue.getKey()
-    Long thisOriginalEstimate = issue.getOriginalEstimate()
-    Long thisTimeSpent = issue.getTimeSpent()
-    Long thisRemainingEstimate = issue.getEstimate()    
-    Long compoundOriginalEstimate
-    Long compoundTimeSpent
-    Long compoundRemainingEstimate
-    String thisStatusName = issue.getStatus().getName()
-	Resolution resolution = issue.getResolution()
-    int childCount = 0
+    // avoiding circularity
+    if (circularityCache.contains(issue) == false) {
+        circularityCache.add(issue)
+    
+        String thisKey = issue.getKey()
+        Long thisOriginalEstimate = issue.getOriginalEstimate()
+        Long thisTimeSpent = issue.getTimeSpent()
+        Long thisRemainingEstimate = issue.getEstimate()    
+        Long compoundOriginalEstimate
+        Long compoundTimeSpent
+        Long compoundRemainingEstimate
+        String thisStatusName = issue.getStatus().getName()
+        Resolution resolution = issue.getResolution()
+        int childCount = 0
 
-    // traversing direct children
-    issueLinkManager.getOutwardLinks(issue.id).each {
-        issueLink ->
-        if (issueLink.issueLinkType.name == "Hierarchy"
-            || issueLink.issueLinkType.name == "Epic-Story Link"
-            || issueLink.issueLinkType.isSubTaskLinkType() == true) {
+        // traversing direct children
+        issueLinkManager.getOutwardLinks(issue.id).each {
+            issueLink ->
+            if (issueLink.issueLinkType.name == "Hierarchy"
+                || issueLink.issueLinkType.name == "Epic-Story Link"
+                || issueLink.issueLinkType.isSubTaskLinkType() == true) {
 
-            childCount++;
-            Issue childIssue = issueLink.getDestinationObject()
-            
-            Long childOriginalEstimate = issue.getOriginalEstimate()
-            Long childTimeSpent = issue.getTimeSpent()
-            Long childRemainingEstimate = issue.getEstimate() 
+                childCount++;
+                Issue childIssue = issueLink.getDestinationObject()
+                
+                Long childOriginalEstimate = issue.getOriginalEstimate()
+                Long childTimeSpent = issue.getTimeSpent()
+                Long childRemainingEstimate = issue.getEstimate() 
 
-            if (childOriginalEstimate != null) {
-                if (compoundOriginalEstimate == null) {
-                    compoundOriginalEstimate = 0
+                if (childOriginalEstimate != null) {
+                    if (compoundOriginalEstimate == null) {
+                        compoundOriginalEstimate = 0
+                    }
+                    compoundOriginalEstimate += childOriginalEstimate
                 }
-                compoundOriginalEstimate += childOriginalEstimate
-            }
 
-            if (childTimeSpent != null) {
-                if (compoundTimeSpent == null) {
-                    compoundTimeSpent = 0
+                if (childTimeSpent != null) {
+                    if (compoundTimeSpent == null) {
+                        compoundTimeSpent = 0
+                    }
+                    compoundTimeSpent += childTimeSpent
                 }
-                compoundTimeSpent += childTimeSpent
-            }
 
-            if (childRemainingEstimate != null) {
-                if (compoundRemainingEstimate == null) {
-                    compoundRemainingEstimate = 0
+                if (childRemainingEstimate != null) {
+                    if (compoundRemainingEstimate == null) {
+                        compoundRemainingEstimate = 0
+                    }
+                    compoundRemainingEstimate += childRemainingEstimate
                 }
-                compoundRemainingEstimate += childRemainingEstimate
-            }
 
-            def customWarningField =  ComponentAccessor.getCustomFieldManager().getCustomFieldObjectByName("Compound Warnings");
-            if(customWarningField != null) {
-                def customAnomalies = childIssue.getCustomFieldValue(customWarningField);
-                if (customAnomalies != null) {
-                    def jsonParser = new JsonSlurper()
-                    def childAnomalies = jsonParser.parseText(customAnomalies.toString())
-                    anomalies.addAll((List<Map<String,String>>) childAnomalies)
+                def customWarningField =  ComponentAccessor.getCustomFieldManager().getCustomFieldObjectByName("Compound Warnings");
+                if(customWarningField != null) {
+                    def customAnomalies = childIssue.getCustomFieldValue(customWarningField);
+                    if (customAnomalies != null) {
+                        def jsonParser = new JsonSlurper()
+                        def childAnomalies = jsonParser.parseText(customAnomalies.toString())
+                        anomalies.addAll((List<Map<String,String>>) childAnomalies)
+                    }
                 }
+
             }
+        }
 
-        }
-    }
-
-    if (childCount == 0) {
-        if (thisOriginalEstimate == null || thisOriginalEstimate == 0) {
-            createAnomaly(anomalies, issue, "WARN", "issue senza original estimate e senza figli")
-        }
-        if (thisRemainingEstimate != null && thisRemainingEstimate > 0 && resolution != null) {
-            createAnomaly(anomalies, issue, "WARN", "issue risolta ma con remaining estimate: il remaining estimate non viene considerato")
-        }
-    } else {
-        if (thisOriginalEstimate != null && thisOriginalEstimate > 0) {
-            createAnomaly(anomalies, issue, "WARN", "issue padre ma con original estimate: l'original estimate non viene considerato, rimuoverlo ed assicurare di averlo distribuito correttamente tra i figli")
-        }
-        if (thisTimeSpent != null && thisTimeSpent > 0) {
-            createAnomaly(anomalies, issue, "WARN", "issue padre ma con worklog: il time spent non viene considerato")
-        }
-        if (thisRemainingEstimate != null && thisRemainingEstimate > 0) {
-            createAnomaly(anomalies, issue, "WARN", "issue padre ma con remaining estimate: il remaining estimate non viene considerato")
+        if (childCount == 0) {
+			if (thisOriginalEstimate == null || thisOriginalEstimate == 0) {
+                createAnomaly(anomalies, issue, "WARN", "issue senza original estimate e senza figli")
+            }
+            if (thisRemainingEstimate != null && thisRemainingEstimate > 0 && resolution != null) {
+                createAnomaly(anomalies, issue, "WARN", "issue risolta ma con remaining estimate: azzerare il remaining sulla issue " + issue.getKey())
+            }
+        } else {
+            if (thisOriginalEstimate != null && thisOriginalEstimate > 0) {
+                createAnomaly(anomalies, issue, "WARN", "issue padre ma con original estimate: rimuovere l'original estimate dalla issue " + issue.getKey() + " assicurandosi di averla distribuita tra i figli")
+            }
+            if (thisTimeSpent != null && thisTimeSpent > 0) {
+                createAnomaly(anomalies, issue, "WARN", "issue padre ma con worklog: rimuovere il worklog dalla issue " + issue.getKey() + " ed inserilo in uno dei figli")
+            }
+            if (thisRemainingEstimate != null && thisRemainingEstimate > 0) {
+                createAnomaly(anomalies, issue, "WARN", "issue padre ma con remaining estimate: rimuovere la remaining estimate dalla issue " + issue.getKey() + " assicurandosi di averla distribuita tra i figli")
+            }
         }
     }
 }
