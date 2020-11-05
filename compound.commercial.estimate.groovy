ea3c6c9751d0e465dde1e2a5ba61931c5ecf044b
Marco Minetti <4608324+marcominetti@users.noreply.github.com>
2020-11-05 23:28:39 +0100

    refactored

--- compound.commercial.estimate.groovy
+++ compound.commercial.estimate.groovy
@@ -1,5 +1,6 @@
 // Number|Number
 enableCache = { ->true }
+def customFieldName = "Compound Commercial Estimate"
 
 import com.atlassian.jira.ComponentManager
 import com.atlassian.jira.component.ComponentAccessor
@@ -13,62 +14,54 @@ import com.atlassian.jira.issue.util.DefaultIssueChangeHolder;
 import org.apache.log4j.Logger
 import org.apache.log4j.Level
 
-def log = Logger.getLogger("COMPUTED")
+def log = Logger.getLogger("SCRIPTED")
 log.setLevel(Level.DEBUG)
 
 def issueLinkManager = ComponentAccessor.getIssueLinkManager()
-def customFieldManager = ComponentAccessor.getCustomFieldManager()
-def circularityCache = []
+def customEstimateField = ComponentAccessor.getCustomFieldManager().getCustomFieldObjectByName(customFieldName);
+Double getCustomFieldValue(Issue issue, CustomField customField) {
+  def customValue
+  if (customField != null) {
+    customValue = issue.getCustomFieldValue(customField);
+  }
+  if (customValue != null) {
+    return (double) customValue
+  }
+  return null
+}
 
-def calculateEstimate(Issue issue, List circularityCache, IssueLinkManager issueLinkManager, CustomFieldManager customFieldManager, Logger log) {
-  Double thisEstimate = 0
+// getting original estimate
+Double thisEstimate = 0
+def estimate = issue.getOriginalEstimate()
+if (estimate > 0) {
+  thisEstimate = (double) estimate / (8 * 3600)
+}
+log.info(String.format("this compound commercial estimate for %s: %s", issue.getKey(), thisEstimate))
 
-  // avoiding circularity
-  if (circularityCache.contains(issue) == false) {
-    circularityCache.add(issue)
-
-    // getting original estimate
-    def estimate = issue.getOriginalEstimate()
-    if (estimate > 0) {
-      thisEstimate = (double) estimate / (8 * 3600)
-    }
-
-    // checking issue type
-    if (issue.getIssueType().getName() == "Milestone" || issue.getIssueType().getName() == "Epic") {
-      if (thisEstimate > 0) {
-        return thisEstimate
-      } else if (issue.getIssueType().getName() == "Milestone") {
-        Double subsEstimate = 0
-        // traversing direct children
-        issueLinkManager.getOutwardLinks(issue.id).each {
-          issueLink ->
-          if (issueLink.issueLinkType.name == "Hierarchy") {
-            Issue childIssue = issueLink.getDestinationObject()
-            if (childIssue.getIssueType().getName() == "Epic") {
-              // reading this custom - scripted - field on child (hopefully triggering deep calculation)
-              Double childEstimate
-
-              def customEstimateField = ComponentAccessor.getCustomFieldManager().getCustomFieldObjectByName("Compound Commercial Estimate");
-              def customEstimate
-              if (customEstimateField != null) {
-                customEstimate = childIssue.getCustomFieldValue(customEstimateField);
-              }
-              if (customEstimate != null) {
-                childEstimate = (double) customEstimate
-              }
-
-              // adding each child estimate
-              if (childEstimate != null) {
-                subsEstimate += childEstimate
-              }
+// checking issue type
+if (issue.getIssueType().getName() == "Milestone" || issue.getIssueType().getName() == "Epic") {
+  if (thisEstimate > 0) {
+    return thisEstimate
+  } else if (issue.getIssueType().getName() == "Milestone") {
+    // traversing direct children
+    Double subsEstimate = 0
+    issueLinkManager.getOutwardLinks(issue.id).each {
+      issueLink ->
+        Issue childIssue = issueLink.getDestinationObject()
+        log.info(String.format("child compound commercial estimate for %s: %s", issue.getKey(), childIssue.getKey()))
+        if (issueLink.issueLinkType.name == "Hierarchy") {
+          if (childIssue.getIssueType().getName() == "Epic") {
+            // reading this custom - scripted - field on child
+            Double childEstimate = getCustomFieldValue(childIssue, customEstimateField)
+            log.info(String.format("child compound commercial estimate for %s: %s", childIssue.getKey(), childEstimate))
+            // adding each child estimate
+            if (childEstimate != null) {
+              subsEstimate += childEstimate
             }
           }
         }
-        return subsEstimate
       }
+      return subsEstimate
     }
-  }
-  return 0
 }
-
-return (Double) calculateEstimate(issue, circularityCache, issueLinkManager, customFieldManager, log)
+return 0
