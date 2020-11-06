fee1b22b11f033da83a64dfe7e5bd4fc2d09c510
Aldi Papa <aldi.papa@linksmt.it>
2020-11-06 10:29:05 +0100

    time spent

--- compound.time.spent.groovy
+++ compound.time.spent.groovy
@@ -1,5 +1,6 @@
 // Number|Number
-enableCache = {-> false}
+enableCache = { ->false
+}
 
 import com.atlassian.jira.ComponentManager
 import com.atlassian.jira.component.ComponentAccessor
@@ -9,64 +10,81 @@ import com.atlassian.jira.issue.CustomFieldManager;
 import com.atlassian.jira.issue.fields.CustomField;
 import com.atlassian.jira.issue.ModifiedValue;
 import com.atlassian.jira.issue.util.DefaultIssueChangeHolder;
-
+import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger
 import org.apache.log4j.Level
 
-def log = Logger.getLogger("COMPUTED")
+def log = Logger.getLogger("SCRIPTED")
 log.setLevel(Level.DEBUG)
 
 def issueLinkManager = ComponentAccessor.getIssueLinkManager()
-def customFieldManager = ComponentAccessor.getCustomFieldManager()
+def customField = ComponentAccessor.getCustomFieldManager().getCustomFieldObjectByName("Compound Time Spent");
 def circularityCache = []
 
-def calculateTimeSpent(Issue issue, List circularityCache, IssueLinkManager issueLinkManager, CustomFieldManager customFieldManager, Logger log) {
-    Double thisTimeSpent = 0
-    Double subsTimeSpent = 0
-    Double compoundTimeSpent = 0
-    
-    // avoiding circularity
-    if (circularityCache.contains(issue) == false) {
-        circularityCache.add(issue)
-        
-        // getting time spent
-        def timespent = issue.getTimeSpent()
-        if (timespent > 0) {
-            thisTimeSpent  = (double) timespent / (8 * 3600)
-        }
-        
-        // traversing direct children
-        issueLinkManager.getOutwardLinks(issue.id).each {
-            issueLink ->
-            if (issueLink.issueLinkType.name == "Hierarchy"
-                || issueLink.issueLinkType.name == "Epic-Story Link"
-                || issueLink.issueLinkType.isSubTaskLinkType() == true) { 
-                                
-                // reading this custom - scripted - field on child (hopefully triggering deep calculation)
-                def childTimeSpent = 0
-                Issue childIssue = issueLink.getDestinationObject()
-                def customTimeSpentField =  ComponentAccessor.getCustomFieldManager().getCustomFieldObjectByName("Compound Time Spent");
-                def customTimeSpent
-                if(customTimeSpentField != null) {
-                	customTimeSpent = childIssue.getCustomFieldValue(customTimeSpentField);
-                } 
-                if (customTimeSpent != null) {
-                	 childTimeSpent = (double) customTimeSpent
-                }
-                
-                // adding each child time spent
-                if (childTimeSpent != null) {
-                    subsTimeSpent += childTimeSpent
-                }
-            }
-        }
-   
-    }
-    
-    // tree compound cumulates over issue time spent
-    compoundTimeSpent = (subsTimeSpent > 0) ? subsTimeSpent : thisTimeSpent
-    
-    return compoundTimeSpent;
+Double getCustomFieldValue(Issue issue, CustomField customField) {
+  def customValue
+  if (customField != null) {
+    customValue = issue.getCustomFieldValue(customField);
+  }
+  if (customValue != null) {
+    return (double) customValue
+  }
+  return 0
 }
 
-return (Double)calculateTimeSpent(issue,circularityCache,issueLinkManager,customFieldManager,log)
+Double calculateTimeSpent(Issue issue, List circularityCache, IssueLinkManager issueLinkManager, CustomField customField, Logger log, Integer level) {
+  def pad = StringUtils.repeat(" ", level)
+  log.info(String.format("%sbegin calculate %s for %s", pad, customField.getName(), issue.getKey()))
+
+  Double result
+
+  // avoiding circularity
+  if (circularityCache.contains(issue) == false) {
+    circularityCache.add(issue)
+
+    // getting time spent
+    Double thisTimeSpent = 0
+    def timespent = issue.getTimeSpent()
+    if (timespent > 0) {
+      thisTimeSpent = (double) timespent / (8 * 3600)
+    }
+
+    log.info(String.format("%sthis compound time spent for %s: %s", pad, issue.getKey(), thisTimeSpent))
+
+    // checking issue type
+    String issueTypeName = issue.getIssueType().getName()
+    switch (issueTypeName) {
+    case "Milestone":
+      if (thisTimeSpent == 0) {
+        // traversing direct children
+        Double subsTimeSpent = 0
+        issueLinkManager.getOutwardLinks(issue.id).each {
+          issueLink ->
+          Issue childIssue = issueLink.getDestinationObject()
+          String childIssueTypeName = childIssue.getIssueType().getName()
+          log.info(String.format("%sprocessing child of %s: %s", pad, issue.getKey(), childIssue.getKey()))
+          if (issueLink.issueLinkType.name == "Hierarchy") {
+            if (childIssue.getIssueType().getName() == "Epic") {
+              Double childTimeSpent = calculateTimeSpent(childIssue, circularityCache, issueLinkManager, customField, log, level + 1)
+              log.info(String.format("%schild compound time spent for %s: %s", pad, childIssue.getKey(), childTimeSpent))
+              subsTimeSpent += childTimeSpent
+            }
+          }
+        }
+        result = subsTimeSpent
+      } else {
+        result = thisTimeSpent
+      }
+      break
+    case "Epic":
+      result = thisTimeSpent
+      break;
+    default:
+      result = 0
+      break;
+    }
+  }
+  log.info(String.format("%send calculate %s for %s: %s", pad, customField.getName(), issue.getKey(), result))
+  return result
+}
+return calculateTimeSpent(issue, circularityCache, issueLinkManager, customField, log, 0)
