fd19193c45c65e52b4a71ef27ada783f9057bc92
Aldi Papa <aldi.papa@linksmt.it>
2020-11-17 08:46:13 +0100

    added n.d for null fields

--- progress.status.groovy
+++ progress.status.groovy
@@ -60,6 +60,13 @@ builder.div (class:"", style: "display: inline-flex; font-weight: 400; max-heigh
         div (class: "", title: "Commercial Estimate", style: "padding: 0px 8px; color: #ffffff; background-color: #999; border-radius: 4px; margin-right: 10px;") {
             span (class: "", String.format((thisCompoundCommercialEstimate*10 < 1) ? "%.2fd" : "%.1fd", thisCompoundOriginalEstimate))
         }
+    }else{
+        div (class: "", style: "padding: 3px 7px; color: #999; background-color: #fff; margin-right: 0px; font-size: 11px; text-overflow: ellipsis; overflow: hidden;") {
+            span ("C ")
+        }
+        div (class: "", title: "Commercial Estimate", style: "padding: 0px 8px; color: #ffffff; background-color: #999; border-radius: 4px; margin-right: 10px;") {
+            span (class: "", "n.d.")
+        }
     }
     if (thisCompoundOriginalEstimate > 0) {
         div (class: "", style: "padding: 3px 7px; color: #89afd7; background-color: #fff; margin-right: 0px; font-size: 11px; text-overflow: ellipsis; overflow: hidden;") {
@@ -67,6 +74,13 @@ builder.div (class:"", style: "display: inline-flex; font-weight: 400; max-heigh
         }
         div (class: "", title: "Original Estimate", style: "padding: 0px 8px; color: #ffffff; background-color: #89afd7; border-radius: 4px; margin-right: 10px;") {
             span (class: "", String.format((thisCompoundOriginalEstimate*10 < 1) ? "%.2fd" : "%.1fd", thisCompoundOriginalEstimate))
+        }
+    }else{
+        div (class: "", style: "padding: 3px 7px; color: #89afd7; background-color: #fff; margin-right: 0px; font-size: 11px; text-overflow: ellipsis; overflow: hidden;") {
+            span ("O ")
+        }
+        div (class: "", title: "Original Estimate", style: "padding: 0px 8px; color: #ffffff; background-color: #89afd7; border-radius: 4px; margin-right: 10px;") {
+            span (class: "", "n.d.")
         }
     }
     if (thisCompoundRemainingEstimate > 0) {
@@ -76,6 +90,13 @@ builder.div (class:"", style: "display: inline-flex; font-weight: 400; max-heigh
         div (class: "", title: "Remaining Estimate", style: "padding: 0px 8px; color: #ffffff; background-color: #ec8e00; border-radius: 4px; margin-right: 10px;") {
             span (class: "", String.format((thisCompoundRemainingEstimate*10 < 1) ? "%.2fd" : "%.1fd", thisCompoundRemainingEstimate))
         }
+    }else{
+        div (class: "", style: "padding: 3px 7px; color: #e2b36b; background-color: #fff; margin-right: 0px; font-size: 11px; text-overflow: ellipsis; overflow: hidden;") {
+            span ("R ")
+        }
+        div (class: "", title: "Remaining Estimate", style: "padding: 0px 8px; color: #ffffff; background-color: #ec8e00; border-radius: 4px; margin-right: 10px;") {
+            span (class: "", "n.d.")
+        }
     }
     if (thisCompoundTimeSpent > 0) {
         div (class: "", style: "padding: 3px 7px; color: #75af59; background-color: #fff; margin-right: 0px; font-size: 11px; text-overflow: ellipsis; overflow: hidden;") {
@@ -83,6 +104,13 @@ builder.div (class:"", style: "display: inline-flex; font-weight: 400; max-heigh
         }
         div (class: "", title: "Logged", style: "padding: 0px 8px; color: #ffffff; background-color: #51a825; border-radius: 4px; margin-right: 10px;") {
             span (class: "", String.format((thisCompoundTimeSpent*10 < 1) ? "%.2fd" : "%.1fd", thisCompoundTimeSpent))
+        }
+    }else{
+        div (class: "", style: "padding: 3px 7px; color: #75af59; background-color: #fff; margin-right: 0px; font-size: 11px; text-overflow: ellipsis; overflow: hidden;") {
+            span ("L ")
+        }
+        div (class: "", title: "Logged", style: "padding: 0px 8px; color: #ffffff; background-color: #51a825; border-radius: 4px; margin-right: 10px;") {
+            span (class: "", "n.d.")
         }
     }
     if (thisCompoundOriginalEstimate > 0 || thisCompoundRemainingEstimate > 0 || thisCompoundTimeSpent > 0) {
@@ -147,6 +175,13 @@ builder.div (class:"", style: "display: inline-flex; font-weight: 400; max-heigh
                     }
                 }
             }
+        }
+    }else{
+        div (class: "", style: "padding: 3px 7px; color: #cccccc; font-size: 11px; text-overflow: ellipsis; overflow: hidden;") {
+            span ("P ")
+        }
+        div (class: "", title: "Progress", style: "padding: 0px 8px; color: #333333; background-color: #eeeeee; border-radius: 4px; margin-right: 10px;") {
+            span (class: "", String.format("%.1f%%", 0 as Double))
         }
     }
 }
